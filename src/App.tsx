9229fde27fc2c0dae90b7c1dc9e6286459381cba
DheenuS <dheendheenu777@gmail.com>
2023-08-18 17:12:26 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Voiceassist from "./Components/voiceassist"
+
+function App() {
+  
+  return (
+      <div>
+        <Voiceassist/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/Components/Voiceassist.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+
+interface PaymentStatus {
+  status: 'success' | 'failure' | null;
+  amount: string | number | null;
+}
+
+function PaymentComponent(){ 
+  const [amount, setAmount] = useState<string>('');
+  const [paymentStatus, setPaymentStatus] = useState<PaymentStatus>({
+    status: null,
+    amount: null,
+  });
+
+  const handlePayment = async (): Promise<void> => {
+    if (+amount <= 0) {
+      announceInvalidAmount();
+      return;
+    }
+
+    // Call payment gateway API to initiate payment
+    try {
+      // Replace this with actual payment gateway integration
+      await simulatePayment();
+
+      setPaymentStatus({ status: 'success', amount });
+      announceTransactionAmount(amount);
+    } catch (error) {
+      setPaymentStatus({ status: 'failure', amount: null });
+    }
+  };
+
+  const announceInvalidAmount = (): void => {
+    if ('speechSynthesis' in window) {
+      const synth = window.speechSynthesis; // convert text to speech via speaker
+      const invalidAmount = new SpeechSynthesisUtterance('Please enter a valid amount and try again!');
+      synth.speak(invalidAmount);
+    }
+  };
+
+  const announceTransactionAmount = (transactionAmount: string): void => {
+    if ('speechSynthesis' in window) {
+      const synth = window.speechSynthesis;
+
+      const rupeesVoice = new SpeechSynthesisUtterance('Payment Successful. Rupees ');
+      const amountVoice = new SpeechSynthesisUtterance(transactionAmount);
+
+      rupeesVoice.voice = synth.getVoices().find(voice => voice.name.includes('English India'));
+      amountVoice.voice = synth.getVoices().find(voice => voice.name.includes('your-preferred-amount-voice'));
+
+      rupeesVoice.onend = () => {
+        synth.speak(amountVoice);
+      };
+
+      synth.speak(rupeesVoice);
+    }
+  };
+
+  return (
+    <div className='flex items-center justify-center bg-[#222] font-bold w-full h-screen'>
+      <div className='w-[500px] h-[346px] bg-white rounded-lg p-4'>
+        <div className='flex flex-col justify-center items-center gap-y-8'>
+          <h2 className='text-[48px]'>Make a Payment</h2>
+          <input
+            type='number'
+            placeholder='Enter amount'
+            value={amount}
+            className='px-4 py-3 rounded-sm bg-sky-100'
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <button
+            className='text-white font-bold bg-sky-400 hover:bg-sky-500 py-3 px-10 rounded-md shadow-lg'
+            onClick={handlePayment}>
+              Pay
+          </button>
+          {paymentStatus.status === 'success' && (
+            <p>
+              Payment successful! Rupees ${paymentStatus.amount}
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+async function simulatePayment(): Promise<void> {
+  // process a delay to process actual API call
+  await new Promise<void>(interval => setTimeout(interval, 2000));
+  // process successful payment
+}
+
+export default PaymentComponent;
